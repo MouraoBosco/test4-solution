b8fd4c79c3e73bafa806a04bb86deb971098d4bc
Mateus Bosco <mateusmouraobosco@hotmail.com>
2025-06-26 09:46:17 -0300

    refactor: now using try catch block and async / wait for improved readability and error handling

--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const router = express.Router();
 const DATA_PATH = path.join(__dirname, '../../../data/items.json'); //Reference to DATA_PATH needed fixing
@@ -10,21 +10,18 @@ let lastModified = null
 let modifiedTime = null
 
 // GET /api/stats
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
 
-  fs.stat(DATA_PATH, (err, info) => {
-    if (err) return next(err);
-    modifiedTime = info.mtimeMs;
-  });
+  try {
+    modifiedTime = (await fs.stat(DATA_PATH)).mtimeMs
 
-  if (stats && modifiedTime == lastModified) {
-    return res.json(stats)
-  }
+    if (stats && modifiedTime == lastModified) {
+      return res.json(stats)
+    }
 
-  fs.readFile(DATA_PATH, (err, raw) => {
-    if (err) return next(err);
+    const raw = await fs.readFile(DATA_PATH);
     const items = JSON.parse(raw);
-    // Intentional heavy CPU calculation
+
     stats = {
       total: items.length,
       averagePrice: statsUtil.mean(items.map(i => i.price)) // now passing containing item prices to utils function
@@ -33,7 +30,10 @@ router.get('/', (req, res, next) => {
     lastModified = modifiedTime
 
     res.json(stats);
-  });
+
+  } catch (err) {
+    next(err)
+  }
 });
 
 module.exports = router;
