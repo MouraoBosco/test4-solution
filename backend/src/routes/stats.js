305dae1479a8ae85fecb442f0cd55a961238f84f
Mateus Bosco <mateusmouraobosco@hotmail.com>
2025-06-26 09:15:35 -0300

    feat: now caching result from get route for endpoint Stats and watching for file changes

--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -5,17 +5,32 @@ const router = express.Router();
 const DATA_PATH = path.join(__dirname, '../../../data/items.json'); //Reference to DATA_PATH needed fixing
 const statsUtil = require('../utils/stats')
 
+let stats = null
+let lastModified = null
+let modifiedTime = null
+
 // GET /api/stats
 router.get('/', (req, res, next) => {
+
+  fs.stat(DATA_PATH, (err, info) => {
+    if (err) return next(err);
+    modifiedTime = info.mtimeMs;
+  });
+
+  if (stats && modifiedTime == lastModified) {
+    return res.json(stats)
+  }
+
   fs.readFile(DATA_PATH, (err, raw) => {
     if (err) return next(err);
-
     const items = JSON.parse(raw);
     // Intentional heavy CPU calculation
-    const stats = {
+    stats = {
       total: items.length,
       averagePrice: statsUtil.mean(items.map(i => i.price)) // now passing containing item prices to utils function
     };
+
+    lastModified = modifiedTime
 
     res.json(stats);
   });
